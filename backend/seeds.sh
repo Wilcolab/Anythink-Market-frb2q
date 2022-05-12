b2fee475e29eea11fda833dd68dc57b9f8acf9f5
Tidhar Klein Orbach <tidhar.o@taboola.com>
2022-05-12 10:10:20 +0300

    seeds.sh scripts with curl

--- backend/seeds.sh
+++ backend/seeds.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
+for i in {21..121}
+do
+#     curl -s localhost/item/item$i
+    curl -X POST -H "Content-Type: application/json" -d "{\"user\":{\"email\":\"john$i@jacob.com\", \"password\":\"johnnyjacob\", \"username\":\"johnjacob$i\"}}" http://localhost:3000/api/users
+    token=$(curl -X POST -H "Content-Type: application/json" -d "{\"user\":{\"email\":\"john$i@jacob.com\", \"password\":\"johnnyjacob\"}}" http://localhost:3000/api/users/login | jq -r '.[].token')
+    echo "
+    index $i, token $token
+    "
+    slug=$(curl --location --request POST 'http://localhost:3000/api/items' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Token $token" \
+    --data-raw "{\"item\":{\"title\":\"item_$i\", \"description\":\"Ever wonder how?\", \"body\":\"Very carefully.\", \"tagList\":[\"dragons\",\"training\"]}}" | jq -r '.[].slug')
 
-yarn seeds
+    echo "
+    Adding comment for item $i slug $slug
+    "
+    curl --location -g --request POST "http://localhost:3000/api/items/$slug/comments" \
+    --header 'Content-Type: application/json' \
+    --header 'X-Requested-With: XMLHttpRequest' \
+    --header "Authorization: Token $token" \
+    --data-raw "{\"comment\":{\"body\":\"Thank you so much $i!\"}}"
+done
+
